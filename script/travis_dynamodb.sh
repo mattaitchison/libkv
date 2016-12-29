85a4719966290c4854ef5915d507492590e69e81
Matt Aitchison <Matt@lanciv.com>
2016-12-29 12:19:09 -0600

    store/dynamodb: fix travis_dynamodb paths

--- script/travis_dynamodb.sh
+++ script/travis_dynamodb.sh
@@ -2,4 +2,4 @@
 
 curl -L http://dynamodb-local.s3-website-us-west-2.amazonaws.com/dynamodb_local_latest.tar.gz -o dynamodb_local_latest.tar.gz
 tar xzvf dynamodb_local_latest.tar.gz
-mv dynamodb_local_latest dynamodb
+# mv DynamoDBLocal_lib dynamodb
